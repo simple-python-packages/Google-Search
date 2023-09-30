fe09f406c55255c0ecd07d3e50b8c9006323d767
Azamat Dosmukhambetov <you@example.com>
2023-10-01 00:35:23 +0500

    fixed: misson successfully complated

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#Siz buni telegram botlarda qo'llashingiz mumkin
 from googlesearch import search
 
 enter = input('nima qidirmoqchisiz:')
 
 for i in search(query=enter, tld='com',num=10,stop=10,pause=2):
     print(i)
+    
+#Github sahifamga obuna bo'ling va star bosing #xaxaxxa
